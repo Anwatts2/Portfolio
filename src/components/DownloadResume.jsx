858f8e0fbc52f3162272e41e5ae4e16455de5d10
Ashley Watts <ashleywatts@Ashleys-MacBook-Pro.local>
2024-08-21 21:11:23 -0500

    8/21

--- src/components/DownloadResume.jsx
+++ src/components/DownloadResume.jsx
@@ -1,10 +1,11 @@
 import { FaDownload } from "react-icons/fa";
+import {Resume} from "../assets/FinTechResume.pdf";
 
 
 const DownloadResume = () => {
   return (
     // Replace the href with the a link to your resume and replace the download attribute with your name
-    <a href="replace_with_link_to_your_resume" download="Ashley Watts" className="flex items-center">
+    <a href={Resume} download="FinTechResume.pdf" className="flex items-center">
       <p className="mr-2">Download Resume</p>
       <FaDownload />
     </a>
